217ac4c67a06b73a894307a4794f253fc79de2fb
Donatas Noreika <donoras@gmail.com>
2023-03-20 09:47:23 +0200

    Pataisytas pabaigtas biudzetas 1.0

--- biudzetas1.py
+++ biudzetas1.py
@@ -16,6 +16,19 @@ class Biudzetas:
         irasas = Irasas(suma, "Pajamos")
         self.zurnalas.append(irasas)
 
+    def prideti_islaidu_irasa(self, suma):
+        irasas = Irasas(suma, "Išlaidos")
+        self.zurnalas.append(irasas)
+
+    def gauti_balansa(self):
+        suma = 0
+        for irasas in self.zurnalas:
+            if irasas.tipas == "Pajamos":
+                suma += irasas.suma
+            if irasas.tipas == "Išlaidos":
+                suma -= irasas.suma
+        return suma
+
     def parodyti_ataskaita(self):
         for irasas in self.zurnalas:
             print(irasas)
@@ -24,11 +37,16 @@ class Biudzetas:
 biudzetas = Biudzetas()
 
 while True:
-    pasirinkimas = int(input("1 - pridėti pajamas\n4 - ataskaita\n0 - išeiti\n"))
+    pasirinkimas = int(input("1 - pridėti pajamas\n2 - pridėti išlaidas\n3 - balansas\n4 - ataskaita\n0 - išeiti\n"))
     match pasirinkimas:
         case 1:
             suma = float(input("Suma: "))
             biudzetas.prideti_pajamu_irasa(suma)
+        case 2:
+            suma = float(input("Suma: "))
+            biudzetas.prideti_islaidu_irasa(suma)
+        case 3:
+            print(biudzetas.gauti_balansa())
         case 4:
             biudzetas.parodyti_ataskaita()
         case 0:
