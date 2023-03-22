96f0df1bdcd8dc898d66b6eaa651ed9a81f92c0e
Donatas Noreika <donoras@gmail.com>
2023-03-22 08:57:30 +0200

    Pridėtas biudzetas 2.2

--- biudzetas2.py
+++ biudzetas2.py
@@ -1,3 +1,5 @@
+import pickle
+
 class Irasas:
     def __init__(self, suma):
         self.suma = suma
@@ -28,15 +30,29 @@ class IslaiduIrasas(Irasas):
 
 class Biudzetas:
     def __init__(self):
-        self.zurnalas = []
+        self.zurnalas = self._gauti_zurnala()
+
+    def _gauti_zurnala(self):
+        try:
+            with open('zurnalas.pkl', 'rb') as file:
+                zurnalas = pickle.load(file)
+        except:
+            zurnalas = []
+        return zurnalas
+
+    def _irasyti_zurnala(self):
+        with open('zurnalas.pkl', 'wb') as file:
+            pickle.dump(self.zurnalas, file)
 
     def prideti_pajamu_irasa(self, suma, siuntejas, info):
         irasas = PajamuIrasas(suma, siuntejas, info)
         self.zurnalas.append(irasas)
+        self._irasyti_zurnala()
 
     def prideti_islaidu_irasa(self, suma, budas, isigyta):
         irasas = IslaiduIrasas(suma, budas, isigyta)
         self.zurnalas.append(irasas)
+        self._irasyti_zurnala()
 
     def gauti_balansa(self):
         suma = 0
